79c9a363fd680f3e4f104e7c67235d05202e5788
Niels Gregersen <nielshtg@hotmail.com>
2019-01-01 19:10:13 +0100

    separate md5 method

--- Program.cs
+++ Program.cs
@@ -123,7 +123,7 @@ namespace ParseCSV
 
                     using (MD5 md5Hash = MD5.Create())
                     {
-                        string hash = GetMd5Hash(md5Hash, w.Word + " " + ww);
+                        string hash = Hashing.GetMd5Hash(md5Hash, w.Word + " " + ww);
 
 
                         if ("e4820b45d2277f3844eac66c903e84be" == hash) {
@@ -149,28 +149,6 @@ namespace ParseCSV
 
 
         }
-        static string GetMd5Hash(MD5 md5Hash, string input)
-        {
-
-            // Convert the input string to a byte array and compute the hash.
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
-            StringBuilder sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
-            for (int i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-
-            // Return the hexadecimal string.
-            return sBuilder.ToString();
-        }
-
-        
 
         static int AsciiSumAnagram () {
             var sum = 0;
